6c46f075bb7f979ef16dffed502ced73a90621d1
Creatorjason <107406861+Creatorjason@users.noreply.github.com>
2023-03-23 16:07:14 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import SearchIcon from "./search.svg"
+import Constants from "./constants"
+import MovieCard from './MovieCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const API_URL = `http://omdbapi.com?apiKey=${Constants.API_KEY}`
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState("")
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+        console.log(data.Search)
+        setMovies(data.Search)
+    }
+
+
+  
+    return (
+        <div className="app">
+            <h1>MoviesArena</h1>
+            <div className="search">
+                <input
+                    placeholder='Search for your favorite movies'
+                    value={searchValue}
+                    onChange={(e) => { 
+                        setSearchValue(e.target.value)
+                     }}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="Search"
+                    onClick={() => {
+                        searchMovies(searchValue)
+                     }}
+                />
+            </div>
+            {
+                movies.length > 0 ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>) :
+                    (<div className="empty">
+                        <h2>Sorry🥺, no movies found😭</h2>
+                    </div>)
+            }
+
+        </div>
+    );
 }
 
-export default App;
+
+export default App
---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+    API_KEY:"f082f9dc",
+    PlaceHolderImg:"https://via.placeholder.com/400"
+
+}
+
+
+export default constants
